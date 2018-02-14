18b95fb54962b2e8edd0ab2a4f65ab1092a61421
tejainece <tejainece@gmail.com>
2018-02-14 14:11:33 +0100

    Bug fix: ClassAdder

--- lib/src/element/setter.dart
+++ lib/src/element/setter.dart
@@ -35,10 +35,10 @@ class ClassAdder implements Setter {
         clazzes.add(class1);
       else if (class1 is List<String>) clazzes.addAll(class1);
     }
-    if (class2 != null) clazzes.add(class1);
-    if (class3 != null) clazzes.add(class1);
-    if (class4 != null) clazzes.add(class1);
-    if (class5 != null) clazzes.add(class1);
+    if (class2 != null) clazzes.add(class2);
+    if (class3 != null) clazzes.add(class3);
+    if (class4 != null) clazzes.add(class4);
+    if (class5 != null) clazzes.add(class5);
   }
 }
 
@@ -50,7 +50,7 @@ typedef bool BoolFunction();
 
 ClassAdder clazzWhen(condition,
     [class1, String class2, String class3, String class4, String class5]) {
-  if (condition is bool) {} else if (condition is BoolFunction) {
+  if (condition is BoolFunction) {
     condition = condition();
   }
   if (condition) return new ClassAdder(class1, class2, class3, class4, class5);
@@ -58,7 +58,7 @@ ClassAdder clazzWhen(condition,
 }
 
 ClassAdder clazzIf(condition, classTrue, [classFalse]) {
-  if (condition is bool) {} else if (condition is BoolFunction) {
+  if (condition is BoolFunction) {
     condition = condition();
   }
   if (condition) return new ClassAdder(classTrue);
